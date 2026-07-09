c97725b542d49f2bf5f60b539ab375f6c4a6bc77
funkygonorheraaa <skibdirizzler4556@gmail.com>
2026-07-08 19:49:38 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 repeat wait() until game:IsLoaded() and not _G.Executed
 _G.Executed = true
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/saucekid/sauceVR/main/modules/Services/PhysicsService.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/funkygonorheraa/holy-skidding-sauce-vr-/main/modules/Services/PhysicsService.lua"))()
 
 --=========[Variables]
 local Players = game:GetService("Players");     
